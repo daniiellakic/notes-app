4f35dced099c7e973ca344a8d00f07d08abd89df
Danijel Lakic <danijellakic@yahoo.com>
2022-10-15 12:55:36 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav>
+            <h1 className='headline'>Make your Notes</h1>
+        </nav>
+    </div>
+  )
+}
+
